299dc942b2beb19eba017ea4baef4b8d86803126
Beshoy Girgis <shoy@1ds.us>
2018-07-31 12:59:39 -0500

    Restore setOverwinter function
    This function is required by z-nomp's node-stratum-pool in order to
    allow both forks that have overwinter as well as forks that don't.

--- src/transaction.js
+++ src/transaction.js
@@ -28,9 +28,13 @@ function Transaction () {
   this.ins = []
   this.outs = []
   this.joinsplits = []
-  this.versionGroupId = '0x03c48270'
-  this.expiry = 0
-  this.zcash = true
+}
+
+Transaction.prototype.setOverwinter = (expiry, versionGroupId, version) => {
+  this.zcash = true;
+  this.version = Math.max((version||3), 3);
+  this.versionGroupId=(versionGroupId||0x03c48270);
+  this.expiry=(expiry||0);
 }
 
 Transaction.DEFAULT_SEQUENCE = 0xffffffff
