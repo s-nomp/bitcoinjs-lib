d9bc2eb0c7ed44981c3a5a7350120d339a58fca0
nettts <40123869+nettts@users.noreply.github.com>
2018-08-04 21:40:07 -0400

    Update transaction.js
    serOverwinter is now = function and not array. This allows it to called in the application.
    
    Reverted two lines back to their original. Needs tested with zcash and we can keep. If it doesn't work with zcash we need to set a if then most likely.

--- src/transaction.js
+++ src/transaction.js
@@ -30,7 +30,7 @@ function Transaction () {
   this.joinsplits = []
 }
 
-Transaction.prototype.setOverwinter = (expiry, versionGroupId, version) => {
+Transaction.prototype.setOverwinter = function (expiry, versionGroupId, version) {
   this.zcash = true;
   this.version = Math.max((version||3), 3);
   this.versionGroupId=(versionGroupId||0x03c48270);
@@ -366,8 +366,13 @@ Transaction.prototype.__byteLength = function (__allowWitness) {
     this.outs.reduce(function (sum, output) { return sum + 8 + varSliceSize(output.script) }, 0) +
     (hasWitnesses ? this.ins.reduce(function (sum, input) { return sum + vectorSize(input.witness) }, 0) : 0) +
     this.joinsplitByteLength() +
-    (this.version === 3 ? 12 : 0)
-  )
+	//need to figure out what the below commented out line does. doesn't work with some coins.
+	//(this.version === 3 ? 12 : 0)
+	//this.versionGroupId and this.expiry are the original lines 
+	//and were removed. added back to stay functional.
+	(this.versionGroupId == null ? 0 : 4) +
+	(this.expiry == null ? 0 : 4)
+   )
 }
 
 Transaction.prototype.clone = function () {
